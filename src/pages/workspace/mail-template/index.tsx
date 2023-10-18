9c40aaaaf57841e475fb8c627606210cd7f61554
wangzezheng <2295908251@qq.com>
2023-10-18 13:26:45 +0800

    添加execConfirm全局弹窗

--- src/pages/workspace/mail-template/index.tsx
+++ src/pages/workspace/mail-template/index.tsx
@@ -3,16 +3,32 @@ import { memo } from 'react';
 //type
 import type { FC } from 'react';
 import styles from './index.module.scss';
+import { execConfirm } from '@/utils/common';
+import { Button } from '@douyinfe/semi-ui';
 interface IProps {
   datas?: any[];
 }
 
 const mailTemplate: FC<IProps> = (props) => {
   const { datas = [] } = props;
+  async function deleteItem() {
+    // 执行删除操作
+    console.log('执行删除操作');
+  }
 
+  const trys = () => {
+    console.log('133');
+
+    execConfirm(deleteItem, undefined, '34');
+  };
   return (
     <div className={styles.mailTemplate}>
-      <div>mailTemplate</div>
+      <div>
+        mailTemplate
+        <div>
+          <Button onClick={trys}>1111 </Button>
+        </div>
+      </div>
     </div>
   );
 };
