40c2355076726d36f863c27acda553fa21bd442c
wangzezheng <2295908251@qq.com>
2023-10-25 22:42:58 +0800

    恢复登录模板的美白切换

--- src/components/Layout/LoginLayout/Layout.tsx
+++ src/components/Layout/LoginLayout/Layout.tsx
@@ -1,12 +1,15 @@
+import { ThemeContextProvider } from '@/store/theme';
 import NavBar from './../FrontLayout/NavBar';
 import styles from './index.module.scss';
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={styles.layout}>
-      <NavBar />
-      <main className={styles.main}>
-        <div className={styles.loginBox}>{children}</div>
-      </main>
-    </div>
+    <ThemeContextProvider>
+      <div className={styles.layout}>
+        <NavBar />
+        <main className={styles.main}>
+          <div className={styles.loginBox}>{children}</div>
+        </main>
+      </div>
+    </ThemeContextProvider>
   );
 }
