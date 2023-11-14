3c220d575d9b26d84971fa1931ee229f7c3898ba
wangzezheng <2295908251@qq.com>
2023-11-14 15:35:52 +0800

    恢复主页和登录的正常样式

--- src/pages/home/statement/index.tsx
+++ src/pages/home/statement/index.tsx
@@ -1,11 +1,4 @@
 import styles from './index.module.scss';
-import { Form, Button, AutoComplete } from '@douyinfe/semi-ui';
-import { useRouter } from 'next/router';
-import { SetStateAction, useState } from 'react';
-import { ToastError, ToastSuccess, ToastWaring } from '@/utils/common';
-import { IconHelpCircle, IconMail, IconUser } from '@douyinfe/semi-icons';
-import VerificationCodeInput from '@/components/VerificationCodeInput';
-import { RegisterByEmail, checkEmail, register } from '@/api/modules/login';
 export default function Statement() {
   return (
     <main className={styles.StatementHome}>
